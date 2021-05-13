7c9450d7f8c820baa856dc9a709845d0cd646046
paula1401 <46165968+paula1401@users.noreply.github.com>
2021-05-13 16:21:14 +0200

    Add files via upload

---
+++ opto.c
@@ -0,0 +1,28 @@
+
+ 
+ #define F_CPU 16000000UL
+
+ #include <stdio.h>
+ #include <avr/io.h>
+ #include <util/delay.h>
+ #include "usart.h"
+
+ int main(void) {
+
+	 uart_init(); // open the communication to the microcontroller
+	 io_redirect(); // redirect input and output to the uart
+
+		 DDRD = 0x00; //Set all PD pins as inputs for the optocoupler (PD7)
+		 PORTD = 0xFF; // Enable pull at PD inputs
+		 if (PIND==0b01111111) // change if PD7 pin is not used
+		 {
+			 _delay_ms(1);
+			 printf("No obstacle");
+		 }
+		 else
+		 {
+			 printf("Obstacle!");
+		 }
+
+	 }
+
