e649f910939b00c52be9ba792edb14786b860018
Sogen <oliver.jorgensen20@gmail.com>
2021-06-01 00:36:44 +0200

    opto_new for main function

---
+++ opto_new.c
@@ -0,0 +1,27 @@
+ #define F_CPU 16000000UL
+
+ #include <stdio.h>
+ #include <avr/io.h>
+ #include <util/delay.h>
+ #include "usart.h"
+
+unsigned char on=0;
+
+ unsigned char opto(void) {
+
+	 uart_init(); // open the communication to the microcontroller
+	 io_redirect(); // redirect input and output to the uart
+
+		 DDRD = 0x00; //Set all PD pins as inputs for the optocoupler (PD7)
+		 PORTD = 0xFF; // Enable pull at PD inputs
+		 if (PIND==0b01111111) // change if PD7 pin is not used
+		 {
+			 return 1;
+		 }
+		 else
+		 {
+			 return 0;
+		 }
+
+	 }
+
